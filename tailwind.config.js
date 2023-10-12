f7d7a9c630c9c1b4109c9845ddf909353a950718
Amru <second.kautsar@gmail.com>
2023-10-12 07:46:21 +0700

    prepare boiler page and feature : set styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      black: "#000",
+      transparent: "transparent",
+      primary: {
+        50: "#fff3f1",
+        100: "#ffe4df",
+        200: "#ffcec5",
+        300: "#ffac9d",
+        400: "#ff7b64",
+        500: "#ff4d2e",
+        600: "#ed3515",
+        700: "#c8290d",
+        800: "#a5250f",
+        900: "#882514",
+        950: "#4b0f04",
+      },
+      secondary: {
+        50: "#f7f7f8",
+        100: "#efeef0",
+        200: "#dbd9de",
+        300: "#bab8c1",
+        400: "#94919f",
+        500: "#777384",
+        600: "#615d6c",
+        700: "#4f4c58",
+        800: "#44414b",
+        900: "#3b3941",
+        950: "#19181b",
       },
     },
+    borderWidth: {
+      1: "1px",
+    },
+    extend: {},
   },
   plugins: [],
-}
+};
