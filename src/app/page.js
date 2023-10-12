cd93faa2c1e020533a2ffa1e3ff3d0567d909fdb
Amru <second.kautsar@gmail.com>
2023-10-12 09:38:05 +0700

    kasih tulisan di homepage

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,15 @@ export default function Page() {
   return (
     <div className="m-8">
       <div className="card">
-        <h1>Heading 1</h1>
-        <h2>Heading 2</h2>
-        <h3>Heading 3</h3>
+        <h1>Belum dibikin nih Homepage nya puh, sepuhh...</h1>
+        <h2>Bantuin kami dong puhhh, aku mah masih pemula</h2>
       </div>
 
       <input type="text" placeholder="Input" />
       <textarea placeholder="Textarea" />
       <select>
-        <option>Option 1</option>
-        <option>Option 2</option>
+        <option>Makan Gorengan</option>
+        <option>Makan Bubur Ayam</option>
       </select>
       <button>Submit</button>
       <button className="btnSecondary">Secondary</button>
