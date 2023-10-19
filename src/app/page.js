12ffdc1d0b8e6e0a3bded83da6b429b3beb3b57b
Amru <second.kautsar@gmail.com>
2023-10-19 13:26:29 +0700

    update : button I Wanna Eat route to dashboard

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,18 @@
+"use client"
+
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
   return (
     <div className="h-screen flex flex-col justify-center items-center space-y-4">
-      <div className="card bg-transparent">
+      <div className=" card bg-transparent">
         <img src="eatYesLogo.png" />
       </div>
       <div>
         <button
           className="btnSecondary w-[250px] h-[50px] text-3xl"
+          onClick={() => router.push("/dashboard")}
         >
           I Wanna Eat!
         </button>
